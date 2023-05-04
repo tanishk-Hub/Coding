abd9d9b18ea2c8f7556291eeec100d560830fb53
tanishk-Hub <132564530+tanishk-Hub@users.noreply.github.com>
2023-05-04 19:45:12 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <b></b>
+      <div className="links">
+        <a href="/"> Home</a>
+        <a href="/create"> About us</a>
+        <a href="/create"> Our new projects </a>
+        
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
